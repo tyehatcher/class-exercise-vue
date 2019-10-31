474806af7acbd57b35e18988275272ec3e0f38a9
tyehatcher <56003121+tyehatcher@users.noreply.github.com>
2019-10-31 16:52:16 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+var app = new Vue({
+  el:"#app",
+  data:{
+    favorites: [{
+      name: 'Water',
+      backgroundC: 'Blue',
+      cals: 0
+    },
+    {
+      name: 'Iced Latte',
+      backgroundC: 'chocolate',
+      cals: 95
+    },
+    {
+      name: 'Strawberry Shake',
+      backgroundC: 'salmon',
+      cals: 550
+    }
+    
+    ]   
+    
+  }
+});
